f5885661022050cb41193fb7086e9f4281f6fdcc
Haining <yhn0535@gmail.com>
2026-05-07 12:21:21 +0800

    feat(v2/sources): export DataSource and SourceResult from package
    sources/__init__.py 暴露 DataSource Protocol 与 SourceResult TypedDict。

--- quant_lab/core/data/sources/__init__.py
+++ quant_lab/core/data/sources/__init__.py
@@ -1,1 +1,8 @@
-"""Per-vendor data source adapters (eastmoney, sina, tencent, akshare, ...)."""
+"""Data source adapters for external APIs."""
+
+from quant_lab.core.data.sources.base import DataSource, SourceResult
+
+__all__ = [
+    "DataSource",
+    "SourceResult",
+]
