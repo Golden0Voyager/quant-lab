e8925e7fa72f6a45a8eb08ee15612949ed2d2b84
Haining <yhn0535@gmail.com>
2026-05-07 12:23:40 +0800

    feat(v2/data): export StockAggregator/DataCacheFacade/DimensionRegistry
    core/data/__init__.py 暴露 3 个数据层主入口供上层使用。

--- quant_lab/core/data/__init__.py
+++ quant_lab/core/data/__init__.py
@@ -1,1 +1,11 @@
 """Data layer: source registry, dimension fetchers, cache, aggregator."""
+
+from quant_lab.core.data.aggregator import StockAggregator
+from quant_lab.core.data.cache import DataCacheFacade
+from quant_lab.core.data.registry import DimensionRegistry
+
+__all__ = [
+    "DataCacheFacade",
+    "DimensionRegistry",
+    "StockAggregator",
+]
