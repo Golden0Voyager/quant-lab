339b61a7bf696d56adef9c39e03158c497fcf6bf
Haining <yhn0535@gmail.com>
2026-05-11 17:32:35 +0800

    feat(v2/dimensions): export new market-tech fetchers
    将 recent_kline、quarterly_trend、industry_compare、top_holders 导出到 dimensions 包公共 API。

--- quant_lab/core/data/dimensions/__init__.py
+++ quant_lab/core/data/dimensions/__init__.py
@@ -2,15 +2,23 @@
 
 from quant_lab.core.data.dimensions.base import DimensionFetcher, safe_fetch
 from quant_lab.core.data.dimensions.consensus import ConsensusFetcher
+from quant_lab.core.data.dimensions.industry_compare import IndustryCompareFetcher
 from quant_lab.core.data.dimensions.performance import PerformanceFetcher
+from quant_lab.core.data.dimensions.quarterly_trend import QuarterlyTrendFetcher
+from quant_lab.core.data.dimensions.recent_kline import RecentKlineFetcher
 from quant_lab.core.data.dimensions.sentiment import SentimentFetcher
+from quant_lab.core.data.dimensions.top_holders import TopHoldersFetcher
 from quant_lab.core.data.dimensions.valuation import ValuationFetcher
 
 __all__ = [
     "ConsensusFetcher",
     "DimensionFetcher",
+    "IndustryCompareFetcher",
     "PerformanceFetcher",
+    "QuarterlyTrendFetcher",
+    "RecentKlineFetcher",
     "safe_fetch",
     "SentimentFetcher",
+    "TopHoldersFetcher",
     "ValuationFetcher",
 ]
